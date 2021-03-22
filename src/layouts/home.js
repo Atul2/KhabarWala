03c6cd5f4c1af9cc470da28fc12f5ea104d9930b
Atul Asrani <atulasrani12@gmail.com>
2021-03-22 08:59:31 +0530

    save all thins are working

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -1,167 +1,160 @@
 import React, { Component, useContext, useState, useEffect } from "react";
 import {
-  Stepper,
-  StepLabel,
-  Step,
-  AppBar,
-  Button,
-  Toolbar,
-  Typography,
-  Box,
+	Stepper,
+	StepLabel,
+	Step,
+	AppBar,
+	Button,
+	Toolbar,
+	Typography,
+	Box,
 } from "@material-ui/core";
 import { FirstStep } from "../components/FirstStep";
 import { SecondStep } from "../components/SecondStep";
 import { multiStepContext } from "../StepContext";
-import { RepeatRounded } from "@material-ui/icons";
+
 import { db, fire } from "../helpers/db";
 import Navigation from "../Navigation";
-import axios from "axios";
+
 import NewsContent from "../components/NewsContent";
-import userEvent from "@testing-library/user-event";
+
 import { withRouter, useHistory } from "react-router-dom";
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Home = ({ user, existuser, category, news, isAuthenticate }) => {
+	const [auth, setAuth] = React.useState(true);
+	const {
+		currentStep,
+		finalData,
+		setSecondStep,
+		docID,
+		secondStep,
+		innerdata,
+		userID,
+		isData,
+	} = useContext(multiStepContext);
+	const [newsArray, setNewsArray] = useState([]);
+	const [newsResults, setNewsResults] = useState();
+	const [loadmore, setLoadmore] = useState(3);
+	const [category1, setCategory1] = useState([]);
+	const [news1, setNews1] = useState([]);
+	const [existuser1, setExist1] = useState("");
+	const history = useHistory();
 
-  const [auth, setAuth] = React.useState(true);
-  const { currentStep, finalData, setSecondStep, docID, secondStep, innerdata, userID, isData } = useContext(multiStepContext);
-  const [newsArray, setNewsArray] = useState([]);
-  const [newsResults, setNewsResults] = useState();
-  const [loadmore, setLoadmore] = useState(3);
-  const [category1, setCategory1] = useState([]);
-  const [news1, setNews1] = useState([]);
-  const [existuser1, setExist1] = useState("");
-  const history = useHistory();
-  console.log("doc id----", docID);
+	useEffect(() => {
+		getUser();
+		fetchApi();
+		isAuthenticate.onAuthentication();
+	}, [userID, category1]);
 
-  useEffect(() => {
-    getUser();
-    fetchApi();
-    isAuthenticate.onAuthentication();
-    // setNewsArray(a.getData);
-    // setNewsResults(a.getData.length);
+	const getUser = () => {
+		if (userID) {
+			db.collection("users_news_category")
+				.where("userId", "==", userID)
+				.onSnapshot((snapshot) => {
+					snapshot.docs.map((doc) => {
+						setExist1(doc.data().userId);
+						setCategory1(doc.data().category);
+						setNews1(doc.data().newsitem);
+					});
+				});
+		} else {
+			console.log("kuch ni mila fir se dekh..le");
+		}
+	};
 
+	//category ["business", "sports","health"]
+	// news ["Hindustan Times", "The Indian Express", "NDTV News"]
 
-  }, [userID, category1]);
+	const fetchApi = async () => {
+		const arr = [];
+		const filterItem = [];
 
-  const getUser = () => {
-    if (userID) {
-      db.collection("users_news_category").where("userId", "==", userID)
-        .onSnapshot((snapshot) => {
-          snapshot.docs.map((doc) => {
-            setExist1(doc.data().userId);
-            setCategory1(doc.data().category);
-            setNews1(doc.data().newsitem);
-          })
-        })
-    } else {
-      console.log("kuch ni mila fir se dekh..le");
-    }
-  }
+		const urls = category1.map((data) => {
+			return fetch(
+				`https://saurav.tech/NewsAPI/top-headlines/category/${data}/in.json`
+			).then((resp) => resp.json());
+		});
 
-  console.log("category 1---", category1);
+		const results = await Promise.all(urls);
+		arr.push(...results);
+		const articles = arr.map((data) => data.articles);
+		const mergeData = filterItem.concat(...articles);
 
-  console.log("News 1---", news1);
+		const getData = mergeData.filter((item) => {
+			return news1.includes(item.source.name);
+		});
 
-  console.log("exist user 1---", existuser1);
+		if (getData.length > 1) {
+			setNewsArray(getData);
+			setNewsResults(getData.length);
+		} else {
+			console.log("home page no data in array ");
+		}
+	};
 
-  //category ["business", "sports","health"]
+	const handleClose = () => {
+		fire
+			.signOut()
+			.then(() => {
+				localStorage.removeItem("user");
 
-  // news ["Hindustan Times", "The Indian Express", "NDTV News"]
+				Navigation.history.replace("/KhabarWala/");
+				window.location.reload();
+			})
+			.catch(console.error);
+	};
 
-  const fetchApi = async () => {
-    const arr = [];
-    const filterItem = [];
+	var showComponent;
 
-    const urls = category1.map((data) => {
-      return fetch(`https://saurav.tech/NewsAPI/top-headlines/category/${data}/in.json`).then(resp => resp.json());
-    });
+	if (userID === existuser1 && docID) {
+		showComponent = (
+			<NewsContent
+				setLoadmore={setLoadmore}
+				loadmore={loadmore}
+				newsArray={newsArray}
+				newsResults={newsResults}
+			/>
+		);
+	} else {
+		if (existuser1 === "" && docID === "") {
+			console.log("home me phle yha a raha h qa");
+			showComponent = <FirstStep />;
+		}
+	}
 
-    const results = await Promise.all(urls);
+	if (secondStep === "secondStep") {
+		showComponent = <SecondStep />;
+	}
+	if (secondStep === "firstStep") {
+		console.log("yha se aaraha h ?");
+		showComponent = <FirstStep />;
+	}
 
-    arr.push(...results);
+	return (
+		<>
+			{auth && (
+				<>
+					<ToastContainer />
+					<div style={{ flexGrow: 1 }}>
+						<AppBar position="sticky">
+							<Toolbar>
+								<Typography variant="h6" style={{ flexGrow: 1 }}>
+									<b>KhabarWala</b>
+								</Typography>
+								<Button color="inherit" onClick={handleClose}>
+									<b>Logout</b>
+								</Button>
+							</Toolbar>
+						</AppBar>
+					</div>
 
-    const articles = arr.map((data) => data.articles);
-
-    const mergeData = filterItem.concat(...articles);
-
-    const getData = mergeData.filter((item) => { return news1.includes(item.source.name) });
-
-    if (getData.length > 1) {
-      setNewsArray(getData);
-      setNewsResults(getData.length);
-    } else {
-      console.log("home page no data in array ");
-    }
-  }
-
-  console.log("news array--", newsArray);
-
-  const handleClose = () => {
-    fire
-      .signOut()
-      .then(() => {
-        localStorage.removeItem("user");
-
-        Navigation.history.replace("/KhabarWala/");
-        window.location.reload();
-      })
-      .catch(console.error);
-  };
-
-  var showComponent;
-
-
-
-  if (userID === existuser1 && docID) {
-    showComponent = <NewsContent
-      setLoadmore={setLoadmore}
-      loadmore={loadmore}
-      newsArray={newsArray}
-      newsResults={newsResults}
-    />;
-  } else {
-    if (existuser1 === '' && docID === '') {
-      console.log("home me phle yha a raha h qa")
-      showComponent = <FirstStep />;
-    }
-  }
-
-  if (secondStep === 'secondStep') {
-    showComponent = <SecondStep />;
-  }
-  if (secondStep === 'firstStep') {
-    console.log("yha se aaraha h ?")
-    showComponent = <FirstStep />;
-  }
-
-
-
-  return (
-    <>
-      {auth && (
-        <>
-          <ToastContainer />
-          <div style={{ flexGrow: 1 }}>
-            <AppBar position="sticky">
-              <Toolbar>
-                <Typography variant="h6" style={{ flexGrow: 1 }}>
-                  <b>KhabarWala</b>
-                </Typography>
-                <Button color="inherit" onClick={handleClose}>
-                  <b>Logout</b>
-                </Button>
-              </Toolbar>
-            </AppBar>
-          </div>
-
-          {showComponent}
-
-        </>
-      )}
-    </>
-  );
+					{showComponent}
+				</>
+			)}
+		</>
+	);
 };
 
 export default withRouter(Home);
